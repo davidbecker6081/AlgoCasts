73d20ac712c1e4edebec6c8fc199b73bcd3bc980
David Becker <david@beckersite.com>
2018-02-23 15:40:41 -0700

    Finish palindrome 3 ways

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,32 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false
+    }
+  }
+  return true
+}
+
+// function palindrome(str) {
+//   const reversed = [...str]
+//     .reverse()
+//     .join('')
+//   return str === reversed
+// }
+
+// function palindrome(str) {
+//   if (str.length < 2) {
+//     return true
+//   }
+//
+//   let letterX = str.slice(0, 1)
+//   let letterY = str.slice(str.length - 1)
+//   let newString = str.slice(1, str.length - 1)
+//
+//   return letterX === letterY ? palindrome(newString) : false
+// }
 
 module.exports = palindrome;

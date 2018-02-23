806013872a3a4afb752e24d3dea906e61ec4ee98
David Becker <david@beckersite.com>
2018-02-23 15:05:22 -0700

    Finish reverse string 3 ways

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+function reverse(str) {
+  let newStr = ''
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i]
+  }
+  return newStr
+}
+
+// function reverse(str) {
+//   let reversed = ''
+//   for (let char of str) {
+//     reversed = char + reversed
+//   }
+//   return reversed
+// }
+
+// function reverse(str) {
+//   return [...str].reduce((rev, char) => char + rev, '')
+// }
 
 module.exports = reverse;

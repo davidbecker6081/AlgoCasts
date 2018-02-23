a9a850bbd73457c1f974e7f95338bc0f4a6a4a48
David Becker <david@beckersite.com>
2018-02-23 16:26:16 -0700

    Finish maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  if (str.length < 2) {
+    return str
+  }
+  let maxCountObject = { letter: '', counter: 0 }
+  const charCounts = [...str].reduce((countObj, char) => {
+    if (!countObj[char]) {
+      countObj[char] = 0
+    }
+    countObj[char] += 1
+
+    return countObj
+  }, {})
+
+  maxCountObject = Object.keys(charCounts).reduce((maxCount, char) => {
+    if (charCounts[char] > maxCount.counter) {
+      maxCount.counter = charCounts[char]
+      maxCount.letter = char
+    }
+    return maxCount
+  }, maxCountObject)
+
+  return maxCountObject.letter
+}
 
 module.exports = maxChar;

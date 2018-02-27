0938f1b6348ddbe3fc5627171268589cd9718837
David Becker <david@beckersite.com>
2018-02-26 22:28:47 -0700

    Finish the anagram method the hard way using ES6 Maps

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,54 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// word.replace(/[^\w]/g, '').toLowerCase()
+// Use a Map for each
+// map.get(key)
+// map.has(key)
+// map.entries
+// map.size
+// map.values
+// map.keys
+
+function anagrams(stringA, stringB) {
+  stringA = stringA.replace(/[^\w]/g, '').toLowerCase()
+  stringB = stringB.replace(/[^\w]/g, '').toLowerCase()
+
+  const mapA = [...stringA].reduce((map, letter) => {
+    if (!map.has(letter)) {
+      map.set(letter, 1)
+    } else {
+      let count = map.get(letter) + 1
+      map.set(letter, count)
+    }
+    return map
+  }, new Map())
+
+  const mapB = [...stringB].reduce((map, letter) => {
+    if (!map.has(letter)) {
+      map.set(letter, 1)
+    } else {
+      let count = map.get(letter) + 1
+      map.set(letter, count)
+    }
+    return map
+  }, new Map())
+
+
+  if (mapA.size === mapB.size) {
+    for (let a_key of mapA.keys()) {
+      if (!mapB.has(a_key)) {
+        return false
+      } else {
+        if (mapB.get(a_key) !== mapA.get(a_key)) {
+          return false
+        }
+      }
+    }
+    return true
+  } else {
+    return false
+  }
+}
 
 module.exports = anagrams;

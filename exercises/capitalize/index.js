18445788f76d584a513d7654bbe3c427f14c52e5
David Becker <david@beckersite.com>
2018-03-01 16:26:21 -0700

    Finish solution to capitalize with slice method

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -19,4 +19,14 @@ function capitalize(str) {
   return newString
 }
 
+function capitalize(str) {
+  const words = []
+
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase + word.slice(1))
+  }
+
+  return words.join(' ')
+}
+
 module.exports = capitalize;

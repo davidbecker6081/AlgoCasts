c84fa5423048a06b3963730a43e4f4ae41f6fc65
David Becker <david@beckersite.com>
2018-03-07 11:27:01 -0700

    Finish recursive solution for pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,23 +13,42 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//
+// function pyramid(n) {
+//   const columnMax = (2 * n) - 1
+//   const midpoint = Math.floor(columnMax / 2)
+//
+//   for (let i = 0; i < n; i++) {
+//     let string = ''
+//
+//     for (let j = 0; j < columnMax; j++) {
+//       if (midpoint - i <= j && midpoint + i >= j) {
+//         string += '#'
+//       } else {
+//         string += ' '
+//       }
+//     }
+//     console.log(string)
+//   }
+// }
 
-function pyramid(n) {
-  const columnMax = (2 * n) - 1
+function pyramid(n, row = 0, level = '') {
+  const columnMax = 2 * n - 1
   const midpoint = Math.floor(columnMax / 2)
 
-  for (let i = 0; i < n; i++) {
-    let string = ''
-
-    for (let j = 0; j < columnMax; j++) {
-      if (midpoint - i <= j && midpoint + i >= j) {
-        string += '#'
-      } else {
-        string += ' '
-      }
-    }
-    console.log(string)
+  if (n === row) {
+    return
   }
+  if (level.length === columnMax) {
+    console.log(level)
+    return pyramid(n, row + 1)
+  }
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    level += '#'
+  } else {
+    level += ' '
+  }
+  pyramid(n, row, level)
 }
 
 module.exports = pyramid;
